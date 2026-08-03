4a949e1b78962e6156e132cfd524677a08669ae3
Arafat Hossan <arafatpjl@gmail.com>
2026-08-03 12:01:26 +1000

    Add Model 2 PJL

---
+++ PacificPortal/Models/PJL.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'G:\SOFT\Software\PJL Software\Web Application\PacificPortal - Copy\PacificPortal\Models\PJL.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
